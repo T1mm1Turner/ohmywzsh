933e536dd0ee4e0577cc2e42fe5b7447f5dc9d7f
T1mm1Turner <danielperez2000@outlook.com>
2023-08-06 14:37:01 -0600

    First commit for personal config

---
+++ custom/autosuggestions-mapping.zsh
@@ -0,0 +1,6 @@
+# Using "showkey -a" to check ascii code keys
+# Formated to C0 control codes 
+## [Ctrl + space]
+bindkey '^ ' autosuggest-accept
+## [Alt + Return]
+bindkey '^[^M' autosuggest-execute
